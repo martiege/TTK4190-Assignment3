23f92e25d5ee4428a1f621392d185e838d99cb37
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2019-11-11 14:46:55 +0100

    Task1.4

---
+++ matlab-code/task1_4/plot_test_hc.m
@@ -0,0 +1,25 @@
+psi_bar = psi - psi_d;
+r_bar = r - r_d; 
+
+figure(1); 
+plot(t, rad2deg(psi), t, rad2deg(psi_d), t, rad2deg(psi_bar)); 
+legend('psi', 'psi_d', 'psi_bar'); 
+xlabel('time [s]'); 
+ylabel('heading [degrees]'); 
+title('Heading'); 
+
+figure(2); 
+plot(t, rad2deg(r), t, rad2deg(r_d), t, rad2deg(r_bar)); 
+legend('r', 'r_d', 'r_bar'); 
+xlabel('time [s]'); 
+ylabel('heading rate [degrees/s]'); 
+title('Heading rate'); 
+
+figure(3); 
+saturation_line = ones(size(t)) * rad2deg(delta_max); 
+plot(t, rad2deg(delta_c), t, saturation_line, t, -saturation_line); 
+ylim([-rad2deg(delta_max), rad2deg(delta_max)]);
+legend('delta_c', 'positive saturation limit', 'negative saturation limit');
+xlabel('time [s]'); 
+ylabel('delta_c [degrees]'); 
+title('Delta_c'); 
