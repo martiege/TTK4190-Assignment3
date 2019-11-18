1bf0f4a54a8ead7e84def2528a807547080f5f79
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2019-11-18 13:06:24 +0100

    updated figures

--- matlab-code/task2_6/compensated.m
+++ matlab-code/task2_6/compensated.m
@@ -1,7 +1,7 @@
 defines;            % defines for the system
 
 tstart=0;           % Sim start time
-tstop=6500;        % Sim stop time
+tstop=7500;        % Sim stop time
 tsamp=10;           % Sampling time for how often states are stored. (NOT ODE solver time step)
                 
 p0=[1000, 700];      % Initial position (NED)
@@ -16,9 +16,9 @@ sim MSFartoystyring_compensated % The measurements from the simulink model are a
 pathplotter(p(:, 1), p(:, 2), psi, tsamp, 20, tstart, tstop, 0, WP); 
 
 figure(3); 
-beta = atan2(v(:, 2), v(:, 1));  % asin(v(:, 2) ./ sqrt(v(:, 1).^2 + v(:, 2).^2)); 
-chi = psi + beta; 
-plot(t, rad2deg(chi), t, rad2deg(psi), t, rad2deg(psi_d), t, rad2deg(beta)); 
+beta_c = atan2(v(:, 2), v(:, 1));  % asin(v(:, 2) ./ sqrt(v(:, 1).^2 + v(:, 2).^2)); 
+chi = psi - beta_c; 
+plot(t, rad2deg(chi), t, rad2deg(psi), t, rad2deg(psi_d), t, rad2deg(beta_c)); 
 legend('Course chi', 'Heading psi', 'Desired course chi_d', 'Crab angle beta'); 
 xlabel('Degrees'); 
 ylabel('Time'); 
